3b56d1c438f4ff13ab9a18c0fce8afd3dd62e92d
halko <hacklabkuopio@protonmail.com>
2023-09-21 21:48:54 +0300

    trigger build

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <Head>
         <Title>Kuopio Hacklab</Title>
         <Meta name="description" content="Kuopion Hacklab jatkaa toimintaa vuonna 2023. Tule mukaan kehittämään Kuopion makerspace-kulttuuria."/>
-  
+    
         <Meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
 
         <Meta property="og:site_name" content="hacklabkuopio.fi"/>
